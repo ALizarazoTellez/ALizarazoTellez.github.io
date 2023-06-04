987dc84405304f53bad9fca3a05f638d0fdb6230
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-06-04 14:22:02 -0500

    configure the menu button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-window.addEventListener("scroll", scrollEvent);
+/* JavaScript. */
 
-let elem = document.querySelector("header");
+// Elements.
+let elem = {
+    "banner": document.querySelector(".banner"),
+    "navbar": document.querySelector(".navbar"),
+    "menu": document.querySelector("#menu"),
+    "aside": document.querySelector(".aside"),
+};
 
-function scrollEvent() {
-    if (window.pageYOffset > elem.getBoundingClientRect().height - 64) {
-        document.querySelector(".navbar").style.top = "0";
+function main() {
+    // Event Handlers.
+    window.addEventListener("scroll", handleScrollEvent);
+    elem.menu.addEventListener("click", toggleMenu);
+}
+
+function handleScrollEvent() {
+    // Show Navbar or hide this.
+    if (window.pageYOffset > elem.banner.getBoundingClientRect().height) {
+        elem.navbar.classList.add("active");
     } else {
-        document.querySelector(".navbar").style.top = "-64px";
+        elem.navbar.classList.remove("active");
     }
 }
+
+function toggleMenu() {
+    if (elem.aside.classList.contains("active")) {
+        elem.aside.classList.remove("active");
+        elem.navbar.classList.remove("menu");
+    } else {
+        elem.aside.classList.add("active");
+        elem.navbar.classList.add("menu");
+    }
+}
+
+main();
