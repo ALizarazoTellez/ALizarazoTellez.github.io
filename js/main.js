70ebea3fe1f2047957f4d1a4ad69b7eb835186e8
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-06-06 12:54:36 -0500

    add support for clear theme
    The links in the menu have also been fixed.
    
    The coding was done with zero planning, which is why it is a bit messy.
    The next commits will consist of fixing the code base.
    
    I also plan to add more text.

--- js/main.js
+++ js/main.js
@@ -7,21 +7,91 @@ let elem = {
     "navbar": document.querySelector(".navbar"),
     "menu": document.querySelector("#menu"),
     "aside": document.querySelector(".aside"),
+    "navZone": document.querySelector("#zone"),
 };
+
+let ancles = [
+    document.querySelector("#home"),
+    document.querySelector("#about"),
+    document.querySelector("#knowledge"),
+    document.querySelector("#education"),
+];
 
 function main() {
     // Event Handlers.
     window.addEventListener("scroll", handleScrollEvent);
     elem.menu.addEventListener("click", toggleMenu);
+
+    for (let e of elem.aside.querySelectorAll("ul li a")) {
+        e.addEventListener("click", () => {
+            setTimeout(toggleMenu, 200);
+        });
+    }
+
+    document.querySelector("#themeToggle").addEventListener("click", toggleTheme);
+}
+
+let lastHight = elem.aside;
+
+function toggleTheme() {
+    let theme = document.querySelector("#theme");
+
+    const dark = "css/themes/catppuccin/mocha.css";
+    const light = "css/themes/catppuccin/latte.css";
+
+    if (theme.getAttribute("href") == dark) {
+        theme.setAttribute("href", light)
+    } else {
+        theme.setAttribute("href", dark)
+    }
 }
 
 function handleScrollEvent() {
     // Show Navbar or hide this.
-    let offset = elem.social.getBoundingClientRect().y + elem.social.getBoundingClientRect().height;
-    if (window.pageYOffset > offset) {
+    if (window.pageYOffset != 0) {
         elem.navbar.classList.add("active");
     } else {
         elem.navbar.classList.remove("active");
+    }
+
+    // Change Navbar Text.
+    let changed = false;
+
+
+    for (let ancle of ancles.toReversed()) {
+        if (ancle.getBoundingClientRect().y < 1) {
+            changed = true;
+            let currentHight = elem.aside
+                .querySelector(`li a[href*='${ancle.getAttribute("id")}']`)
+                .parentElement
+
+            if (currentHight == lastHight) {
+                break;
+            }
+            lastHight.classList.remove("active");
+
+            lastHight = currentHight;
+            currentHight.classList.add("active");
+
+            elem.navZone.classList.add("hide");
+            setTimeout(() => {
+                elem.navZone.innerText = ancle.innerText;
+            }, 400);
+            setTimeout(() => {
+                elem.navZone.classList.remove("hide");
+            }, 400);
+            break;
+        }
+    }
+
+    if (!changed && elem.navZone.innerText != "PERFIL") {
+        elem.navZone.classList.add("hide");
+        setTimeout(() => {
+            elem.navZone.innerText = "Perfil";
+        }, 400);
+        setTimeout(() => {
+            elem.navZone.classList.remove("hide");
+        }, 400);
     }
 }
 
